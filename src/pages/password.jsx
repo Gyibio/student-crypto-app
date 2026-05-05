05d572c2b978851b498c6b31b15af9b340d31211
Nana Kwame Adom-Boateng <royadom480@gamil.com>
2026-05-05 00:35:18 +0000

    fix imports

--- src/pages/password.jsx
+++ src/pages/password.jsx
@@ -16,7 +16,7 @@ export default function Password() {
     e.preventDefault();
     try {
       const response = await axios.post(
-        `${import.meta.env.VITE_API_URL}/api/auth/register`,
+        `${import.meta.env.VITE_API_URL}/api/auth/register`, 
         { name, email, password },
       );
       if (response.status === 201) {
