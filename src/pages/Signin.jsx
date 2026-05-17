adbea643bf4a680954e401abec5a2044377d375e
Nana Kwame Adom-Boateng <royadom480@gamil.com>
2026-05-17 14:01:21 +0000

    add 'show password feature'

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -9,56 +9,55 @@ export default function Signin() {
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const [showPassword, setShowPasswors] = useState(false);
 
-const handleSignin = async (e) => {
-  e.preventDefault();
-  setLoading(true);
-  try {
-    const response = await axios.post(
-      `${import.meta.env.VITE_API_URL}/api/auth/login`,
-      { email, password }
-    );
+  const handleSignin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/api/auth/login`,
+        { email, password },
+      );
 
-    console.log("Full Backend Response:", response.data); // IMPORTANT: Check this in browser console
+      console.log("Full Backend Response:", response.data); // IMPORTANT: Check this in browser console
 
-    if (response.status === 200) {
-      // Extract data carefully
-      const token = response.data.token;
-      const userData = response.data.user;
+      if (response.status === 200) {
+        // Extract data carefully
+        const token = response.data.token;
+        const userData = response.data.user;
 
-      if (token) {
-        localStorage.setItem("token", token);
-        console.log("Token Saved successfully")
-      }else {
-        console.error("Token was undefined in response data")
-      }
+        if (token) {
+          localStorage.setItem("token", token);
+          console.log("Token Saved successfully");
+        } else {
+          console.error("Token was undefined in response data");
+        }
         // Only stringify if user exists to prevent errors
         if (userData) {
           localStorage.setItem("user", JSON.stringify(userData));
         }
 
         alert("Signin Successful");
-        setTimeout(()=> {
-        navigate("/profile");
-
-        }, 100)
+        setTimeout(() => {
+          navigate("/profile");
+        }, 100);
       } else {
         alert("Server did not return a token. Check backend deployment.");
       }
+    } catch (error) {
+      console.error("Login Error Details:", error.response?.data);
+      const message = error.response?.data || "Login failed.";
+      alert(message);
+    } finally {
+      setLoading(false);
     }
-  catch (error) {
-    console.error("Login Error Details:", error.response?.data);
-    const message = error.response?.data || "Login failed.";
-    alert(message);
-  } finally {
-    setLoading(false);
-  }
-};  
+  };
   if (loading) {
     return <LoadingSpinner message="Connecting to secure vault..." />;
   }
 
-return (
+  return (
     <div className="min-h-screen bg-black text-white flex flex-col items-center pt-8 px-6 font-sans">
       {/* Header with White "C" Logo */}
       <div className="w-full max-w-7xl mb-24">
@@ -81,15 +80,61 @@ return (
             className="w-full bg-black border border-[#2d2d2d] rounded-lg p-4 text-white focus:border-[#0052FF] focus:ring-1 focus:ring-[#0052FF] outline-none transition-all placeholder:text-[#575757]"
           />
           <label className="text-sm font-bold">Password</label>
-          <input
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter password"
-            className="w-full bg-black border border-[#2d2d2d] rounded-lg p-4 text-white focus:border-[#0052FF] focus:ring-1 focus:ring-[#0052FF] outline-none transition-all placeholder:text-[#575757]"
-          />
-          <button 
-          disabled={loading}
-          className={`w-full bg-[#1e2b4d] text-[#4d66a8] py-4 rounded-full font-bold text-lg mb-8 ${loading ? "bg-gray-800 text-gray-500 cursor-not-allowed" : "bg-[#1e2b4d] text-[#4d66a8]"}`}>
+          <div>
+            <input
+              type={showPassword ? "text" : "password"}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter password"
+              className="w-full bg-black border border-[#2d2d2d] rounded-lg p-4 text-white focus:border-[#0052FF] focus:ring-1 focus:ring-[#0052FF] outline-none transition-all placeholder:text-[#575757]"
+            />
+
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-500 hover:text-white transition-colors"
+            >
+              {showPassword ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+          <button
+            disabled={loading}
+            className={`w-full bg-[#1e2b4d] text-[#4d66a8] py-4 rounded-full font-bold text-lg mb-8 ${loading ? "bg-gray-800 text-gray-500 cursor-not-allowed" : "bg-[#1e2b4d] text-[#4d66a8]"}`}
+          >
             {loading ? "verifying..." : "Continue"}
           </button>
         </form>
